62a9dad25bf5d7c525ac9d304af55407a9893c62
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-03-21 18:03:51 +0100

    Adding kicking ball up function

--- src/index.js
+++ src/index.js
@@ -25,17 +25,43 @@ function startPlaying(){
   fallingBall();
 }
 
+
+function kickBall(){
+  const child = document.getElementsByClassName('ball_wrapper');
+
+  let i=0;
+  let id2 = setInterval(goUp,8);
+  function goUp(){
+    if(i<50){
+      const currentPix = parseInt(child[0].style.top) - 3;
+      // console.log("top:",parseInt(child[0].style.top));
+      // console.log("sth",sth);
+      // console.log("currentPix",currentPix);
+      child[0].setAttribute("style",`left:45%;top:${currentPix}px;`);
+      i++;
+    } else {
+      clearInterval(id2);
+    }
+  }
+}
+
 function fallingBall(){
   const child = document.getElementsByClassName('ball_wrapper');
-  // console.log(window.innerHeight-35);
-  let sth=0;
-  let id = setInterval(movingBall,3);
-
+  // let ballGoesUp = 0;
+  // function kickBall(){
+  //   ballGoesUp=50;
+  // }
+  let currentPix=0;
+  let id = setInterval(movingBall,8);
   function movingBall(){
-    console.log(sth);
-    if(sth<window.innerHeight-35){
-      child[0].setAttribute("style",`left:45%;top:${sth}px;`);
-      sth++;
+    // console.log("currentPix",currentPix, window.innerHeight-65);
+    let currentPix=parseInt(child[0].style.top);
+    if(currentPix<window.innerHeight-65){
+      const updatedPix = parseInt(child[0].style.top) + 1;
+      // console.log("top:",parseInt(child[0].style.top));
+      // console.log("sth",sth);
+      // console.log("currentPix",currentPix, window.innerHeight-65);
+      child[0].setAttribute("style",`left:45%;top:${updatedPix}px;`);
     } else {
       clearInterval(id);
       const button = document.createElement('button');
@@ -54,8 +80,11 @@ function stopFalling(){
     clearInterval(initialInterval);
 }
 
+
+
 function ball() {
   const ball = document.createElement('div');
+  ball.addEventListener('click',kickBall);
   ball.classList.add('ball_wrapper');
   ball.setAttribute("style","left:45%;top:0px;")
   return ball;
